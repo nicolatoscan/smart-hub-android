1bfbbb82bd4d268c2bec0d26378e792b0207e0b6
Nicola Toscan <nicolatoscan99@outlook.it>
2020-11-01 03:19:28 +0100

    first commit

---
+++ app/src/main/java/dev/nicolatoscan/smarthub/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.nicolatoscan.smarthub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
